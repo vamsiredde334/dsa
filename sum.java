bce792909fc47d9e8e1fdc9bc61704ea50d49256
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-07-10 21:50:31 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sum {
+    static int fib(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the fibbonaci of you want : ");
+        int n=sc.nextInt();
+        System.out.println("the fibbonaci digit of "+n+" is "+fib(n));
+    }
+}
