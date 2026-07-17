074edf5789fa47fe80055dc8b1f954af9c4f0493
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-07-17 21:15:50 +0530

    Add files via upload

--- filewritterdemo.java
+++ filewritterdemo.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 public class filewritterdemo {
     public static void main(String[] args) throws IOException {
         FileWriter fileWriter=new FileWriter("student.txt",true);
-        fileWriter.write("VAMC REDDY");
-        fileWriter.write("VAMC REDDY");
+        fileWriter.write("vamsi/n");
+        fileWriter.write("rahul/n");
+        fileWriter.write("rahul/n");
 
         fileWriter.close();;
         System.out.println("data writen sucessfully");
